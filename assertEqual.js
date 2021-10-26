f55cc7bca24dba1403053149450d4224ae537575
Mojdehh <mojdeh.sbzgh@gmail.com>
2021-10-26 01:01:13 +0000

    assert function

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('✅✅✅ Assertio Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log('🟥🟥🟥Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Mojdeh", "Mojdeh");
+assertEqual(1, 12);
