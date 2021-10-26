e2c82c0fa41d044b118d0b214cf28168120a6a2a
Mojdehh <mojdeh.sbzgh@gmail.com>
2021-10-26 01:21:20 +0000

    using template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log('✅✅✅ Assertio Passed: ' + actual + ' === ' + expected);
+    console.log(`✅✅✅ Assertio Passed: ${actual} === ${expected}`);
   } else {
-    console.log('🟥🟥🟥Assertion Failed: ' + actual + ' !== ' + expected);
+    console.log(`🟥🟥🟥Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
