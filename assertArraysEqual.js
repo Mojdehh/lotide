5969b5bbd6b682bd7509b0b960e404dd5b64f3aa
Mojdehh <mojdeh.sbzgh@gmail.com>
2021-10-27 13:58:33 +0000

    assertion of two equal arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,19 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    } 
+  }
+  return true;
+}
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${JSON.stringify(arr1)} === ${JSON.stringify(arr2)}`);
+  } else {
+    console.log(`🟥🟥🟥 Assertion Failed: ${JSON.stringify(arr1)} !== ${JSON.stringify(arr2)}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3, "4"], [1, 2, 3, 4]);
