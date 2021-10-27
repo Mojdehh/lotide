e45aea27629b7838751ea0a6ed2cd72fd52da1f1
Mojdehh <mojdeh.sbzgh@gmail.com>
2021-10-27 13:57:05 +0000

    modify for non-equal length

--- eqArrays.js
+++ eqArrays.js
@@ -7,6 +7,7 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
@@ -15,7 +16,7 @@ const eqArrays = function(arr1, arr2) {
   return true;
 }
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+assertEqual(eqArrays([1, 2, 3], [1, 2]), true); 
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true)
