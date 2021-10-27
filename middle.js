f0c0309dc36825ac555118a06216b82733b6f925
Mojdeh Sabz <mojdeh.sbzgh@gmail.com>
2021-10-27 19:04:04 -0400

    return the most middle elements

---
+++ middle.js
@@ -0,0 +1,36 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${JSON.stringify(arr1)} === ${JSON.stringify(arr2)}`);
+  } else {
+    console.log(`🟥🟥🟥 Assertion Failed: ${JSON.stringify(arr1)} !== ${JSON.stringify(arr2)}`);
+  }
+};
+
+const middle = function(array) {
+  let middleOfArray = [];
+  if (array.length === 1 || array.length === 2) {
+    return [];
+  } else if (array.length % 2 !== 0) {
+    middleOfArray.push(array[(array.length - 1) / 2]);
+    return middleOfArray;
+  } else if (array.length % 2 === 0) {
+    middleOfArray.push(array[(array.length / 2) - 1], array[(array.length / 2)]);
+    return middleOfArray;
+  }
+
+};
+
+
+console.log(middle([1, 2])); // => []
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
