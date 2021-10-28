39340f16ea8525db4b983cf2ad6167ed1a6030f6
Mojdeh Sabz <mojdeh.sbzgh@gmail.com>
2021-10-28 19:07:21 -0400

    find the key based on given value

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertio Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🟥🟥🟥Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKeyByValue = function(obj, value) {
+  for (let key in obj) {
+    if ( obj[key] === value ) {
+      return key
+    } // else {
+     // return undefined
+   // }
+  }
+}
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
