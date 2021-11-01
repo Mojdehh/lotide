cfad92794b71e1303eb68975ad061d3e6c900e71
Mojdeh Sabz <mojdeh.sbzgh@gmail.com>
2021-11-01 15:30:12 -0400

    seperate middle function  test files

--- middle.js
+++ middle.js
@@ -1,20 +1,21 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+// const eqArrays = function(arr1, arr2) {
+//   if (arr1.length !== arr2.length) return false;
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] !== arr2[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
 
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2) === true) {
-    console.log(`✅✅✅ Assertion Passed: ${JSON.stringify(arr1)} === ${JSON.stringify(arr2)}`);
-  } else {
-    console.log(`🟥🟥🟥 Assertion Failed: ${JSON.stringify(arr1)} !== ${JSON.stringify(arr2)}`);
-  }
-};
+// const assertArraysEqual = function(arr1, arr2) {
+//   if (eqArrays(arr1, arr2) === true) {
+//     console.log(`✅✅✅ Assertion Passed: ${JSON.stringify(arr1)} === ${JSON.stringify(arr2)}`);
+//   } else {
+//     console.log(`🟥🟥🟥 Assertion Failed: ${JSON.stringify(arr1)} !== ${JSON.stringify(arr2)}`);
+//   }
+// };
+
 
 const middle = function(array) {
   let middleOfArray = [];
@@ -30,7 +31,9 @@ const middle = function(array) {
 
 };
 
+module.exports = middle;
 
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
+
+// assertArraysEqual(middle([1, 2]), []);
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
